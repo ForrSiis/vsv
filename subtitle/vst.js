a8084f522e6201a3b95544fba8c29ae53fe57e29
Shenafu <coder@ieants.cc>
2020-10-15 01:10:47 -0700

    ; javascript ; export rows

---
+++ subtitle/vst.js
@@ -0,0 +1,9 @@
+
+(function() {
+// Change the second argument to your options:
+// https://github.com/sampotts/plyr/#options
+const player = new Plyr('video', {captions: {active: true}});
+
+// Expose player so it can be used from the console
+window.player = player;
+})();
